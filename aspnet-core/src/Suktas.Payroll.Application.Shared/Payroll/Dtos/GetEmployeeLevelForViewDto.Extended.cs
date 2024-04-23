513e34e7767359750ace1c4c3ecffd3fbf37a4c0
chiranshrestha <chiranshrestha20@gmail.com>
2024-04-23 14:37:57 +0545

    dug  fixed

--- aspnet-core/src/Suktas.Payroll.Application.Shared/Payroll/Dtos/GetEmployeeLevelForViewDto.Extended.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Suktas.Payroll.Payroll.Dtos
-{
-    public class GetEmployeeLevelForViewDto : GetEmployeeLevelForViewDtoBase
-    {
-        // Write your custom code here. 
-        // ASP.NET Zero Power Tools will not overwrite this class when you regenerate the related entity.        
-    }
-}
